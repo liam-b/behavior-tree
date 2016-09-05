3205500fc33f93f34f14d85c4d04d90f233cf39a
Liam Brennan <liam@anywhich.com>
2016-09-05 17:18:54 +1000

    new thingo

--- characterBased/main.js
+++ characterBased/main.js
@@ -1,15 +1,5 @@
-const tree = require('./tree.js')
-
-Array.prototype.remove = function (index, index2) {
-  if (!index2) index2 = index
-  return this.splice(index, index2 - index + 1)
-}
-Array.prototype.insert = function(i, ...rest) {
-  return this.splice(i, 0, ...rest)
-}
-Array.prototype.contains = function(value) {
-  return this.indexOf(value) != -1
-}
+var tree = require('./tree.js')
+require('./proto.js')
 
 function randInt (min, max) {
   return Math.floor((Math.random() * (max + 1)) + min)
@@ -29,14 +19,14 @@ function random (length) {
 
 // console.log(random(10))
 
-// console.log(tree.run([1, 1, 1], ['c', ['c']]))
-var loop = true
+console.log(tree.run([1, 1, 1], ['add', ['subtract', 10, 5], 1]))
+// var loop = true
 
-while (loop) {
-  var rand = random(4)
-  var currentDna = tree.parse(rand)
-  console.log(currentDna)
-  var result = tree.run([1], currentDna)
-  console.log(result)
-  if (result == 2) loop = false
-}
+// while (loop) {
+//   var rand = random(4)
+//   var currentDna = tree.parse(rand)
+//   console.log(currentDna)
+//   var result = tree.run([1], currentDna)
+//   console.log(result)
+//   if (result == 2) loop = false
+// }
