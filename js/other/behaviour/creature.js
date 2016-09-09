3a9af8fc880a2a38cda5766800ac81df7b162043
Liam Brennan <liam@anywhich.com>
2016-09-09 16:54:59 +1000

    umm, pulling errors?

--- js/other/behaviour/creature.js
+++ js/other/behaviour/creature.js
@@ -3,7 +3,6 @@ var Properties = require('./properties.js')
 class Creature {
 	constructor (info, behavior) {
 		this.behavior = behavior
-<<<<<<< HEAD
 		this.entity = new Entity(info)
 		this.iter = 0
 	}
@@ -13,7 +12,6 @@ class Creature {
 			this.behavior[i].run(this.entity)
 			console.log(this.entity.log())
 	  }
-=======
 		this.properties = new Properties(info)
 	}
 
@@ -21,7 +19,6 @@ class Creature {
 		for (var i in this.behavior) {
 			this.behavior[i].run(this.properties)
 		}
->>>>>>> 8a4601e6a930bdc422a9ac5c9aa12c9a3ac39c34
 	}
 
 	behave () {
