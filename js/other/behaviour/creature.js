531c0de3958c1be6480796770fb27cbf5725760e
Liam Brennan <liam@anywhich.com>
2016-10-02 20:41:16 +1100

    nearly working evolution on backend

--- js/other/behaviour/creature.js
+++ js/other/behaviour/creature.js
@@ -1,34 +1,60 @@
 var Properties = require('./properties.js')
 
-class Creature {
-  constructor (info, behavior) {
-    this.behavior = behavior
-    this.properties = new Properties(info)
-    this.iter = 0
-  }
+// class Creature {
+//   constructor (info, behavior) {
+//     this.behavior = behavior
+//     this.properties = new Properties(info)
+//     this.iter = 0
+//   }
+//
+//   behaveLoop () {
+//     for (var i = 0; i < this.behavior.length; i += 1) {
+//       this.behavior[i].run(this.properties)
+//       console.log(this.properties.log())
+//     }
+//   }
+//
+//   fitness () {
+//     this.properties.fitness = (Math.abs(4 - this.properties.position.x) + Math.abs(4 - this.properties.position.y)) * (this.properties.energy / 30)
+//     return this.properties.fitness;
+//   }
+//
+//   behave () {
+//     if (this.iter >= this.behavior.length) {
+//       this.finishedBehaving = true
+//     }
+//     else {
+//       this.behavior[this.iter].run(this.properties)
+//       this.iter += 1
+//     }
+//     //console.log('info', this.properties.log())
+//   }
+// }
 
-  behaveLoop () {
+module.exports = function (info, behavior) {
+  this.behavior = behavior
+  this.properties = new Properties(info)
+  this.iter = 0
+
+  this.behaveLoop = function () {
     for (var i = 0; i < this.behavior.length; i += 1) {
       this.behavior[i].run(this.properties)
       console.log(this.properties.log())
     }
   }
 
-  fitness () {
-    this.properties.fitness = (Math.abs(4 - creature.properties.position.x) + Math.abs(4 - creature.properties.position.y)) * (creature.properties.energy / 30)
-    return this.properties.fitness;
+  this.evaluateFitness = function () {
+    return (Math.abs(4 - this.properties.position.x) + Math.abs(4 - this.properties.position.y)) * (this.properties.energy / 30)
   }
 
-  behave () {
-    if (!(this.iter >= this.behavior.length)) {
-      this.behavior[this.iter].run(this.properties)
-      this.iter += 1
+  this.behave = function () {
+    if (this.iter >= this.behavior.length) {
+      this.finishedBehaving = true
     }
     else {
-      this.finishedBehaving = true
+      this.behavior[this.iter].run(this.properties)
+      this.iter += 1
     }
     //console.log('info', this.properties.log())
   }
 }
-
-module.exports = Creature
--- js/other/behaviour/evolution.js
+++ js/other/behaviour/evolution.js
@@ -6,8 +6,8 @@ module.exports = function (settings) {
   this.creatures = []
   this.generation = 0
 
-  this.populate = function (count) {
-    for (var i = 0; i < count; i += 1) {
+  this.populate = function () {
+    for (var i = 0; i < this.settings.populationCount; i += 1) {
       this.creatures.push(new Creature ({
         energy: this.settings.generation.energy,
         rotation: this.settings.generation.rotation,
@@ -24,10 +24,10 @@ module.exports = function (settings) {
 
   this.grade = function () {
     for (var creature in this.creatures) {
-      while (!this.creatures[creature].properties.finishedBehaving) {
-        this.creatures[creature].behave()
-      }
-      this.creatures[creature].fitness()
+      // while (!this.creatures[creature].properties.finishedBehaving) {
+      //   this.creatures[creature].behave()
+      // }
+      this.creatures[creature].fitness = this.creatures[creature].evaluateFitness()
     }
   }
 
@@ -37,5 +37,70 @@ module.exports = function (settings) {
       if (a.properties.fitness < b.properties.fitness) return -1;
       return 0
     })
+
+    let cullBelow = settings.cullPercent / 100 * this.creatures.length
+
+    for (var creature = this.creatures.length; creature > cullBelow; creature -= 1) {
+      this.creatures.splice(creature, 1)
+    }
+  }
+
+  this.repopulate = function () {
+    while (this.creatures.length < this.settings.populationCount) {
+      for (var newCreature in this.creatures) {
+        if (newCreature + this.creatures.length >= this.settings.populationCount) break
+        this.creatures[newCreature].properties = {
+          energy: this.settings.generation.energy,
+          rotation: this.settings.generation.rotation,
+          position: {
+            x: this.settings.generation.x,
+            y: this.settings.generation.y
+          }
+        }
+        this.creatures.push(new Creature ({
+          energy: this.settings.generation.energy,
+          rotation: this.settings.generation.rotation,
+          position: {
+            x: this.settings.generation.x,
+            y: this.settings.generation.y
+          }
+        }, mutation.change({
+          'modify': this.settings.mutation.modify,
+          'modifyRange': this.settings.mutation.modifyRange,
+          'change': this.settings.mutation.change,
+          'edit': this.settings.mutation.edit
+        }, this.creatures[newCreature].behavior)))
+      }
+    }
   }
 }
+
+var test = new module.exports ({
+  'generation': {
+    'energy': 100,
+    'rotation': 0,
+    'position': {
+      'x': 0,
+      'y': 0
+    },
+    'mutation': {
+      'min': 4,
+      'max': 10
+    }
+  },
+
+  'cullPercent': 75,
+  'populationCount': 100,
+
+  'mutation': {
+    'modify': 30,
+    'modifyRange': 2,
+    'change': 20,
+    'edit': 10
+  }
+})
+
+test.populate()
+test.grade()
+
+console.log(test)
